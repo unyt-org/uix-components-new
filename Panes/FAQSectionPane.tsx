a8c88a8bfe565067e2559ad26be7151f500c8a82
Jonas Strehle <jonas@strehles.info>
2023-11-12 14:44:59 +0100

    Update FAQSectionPane.tsx

--- Panes/FAQSectionPane.tsx
+++ Panes/FAQSectionPane.tsx
@@ -1,6 +1,6 @@
 import { template } from "uix/html/template.ts";
 import { Component } from "uix/components/Component.ts";
-import Markdown from "unyt/components/Markdown.tsx";
+import Markdown from "../Defaults/Markdown.tsx";
 
 export type FAQSectionPaneItem = {
 	title: string,
