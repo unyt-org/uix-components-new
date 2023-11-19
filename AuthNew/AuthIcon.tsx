ed1ee5ea2c087e178d7904c300443e345dd92f11
Jonas Strehle <jonas@strehles.info>
2023-11-19 15:27:18 +0100

    Remove font awesome icon from auth component

--- AuthNew/AuthIcon.tsx
+++ AuthNew/AuthIcon.tsx
@@ -17,7 +17,9 @@ import { include } from "uix/base/decorators.ts";
 			<div class="spinner"></div>
 			<img class="toggle" src="https://cdn.unyt.org/unyt-resources/logos/unyt/round-dark-background.png"/>
 		</a>
-		<span class="toggle fa fa-chevron-down"/>
+		<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="toggle chevron-down" viewBox="0 0 16 16" stroke-width="2">
+			<path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
+		</svg>
 		<div id="backdrop"/>
 		<iframe id="iframe" allow="clipboard-write" allowtransparency="true"/>
 	</>
