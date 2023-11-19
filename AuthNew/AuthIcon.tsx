dd54a239200009031338734acd83a42dadb72fb1
Jonas Strehle <jonas@strehles.info>
2023-11-19 15:35:15 +0100

    Fix icon

--- AuthNew/AuthIcon.tsx
+++ AuthNew/AuthIcon.tsx
@@ -17,8 +17,8 @@ import { include } from "uix/base/decorators.ts";
 			<div class="spinner"></div>
 			<img class="toggle" src="https://cdn.unyt.org/unyt-resources/logos/unyt/round-dark-background.png"/>
 		</a>
-		<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="toggle chevron-down" viewBox="0 0 16 16" stroke-width="2">
-			<path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
+		<svg xmlns="http://www.w3.org/2000/svg" class="toggle chevron-down" width="20" height="20" viewBox="0 0 512 512">
+			<path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/>
 		</svg>
 		<div id="backdrop"/>
 		<iframe id="iframe" allow="clipboard-write" allowtransparency="true"/>
