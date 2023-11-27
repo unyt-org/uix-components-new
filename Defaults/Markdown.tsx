cc74c7cb7f7efdf4400e53834b7fad284909f30a
Jonas Strehle <jonas@strehles.info>
2023-11-27 20:38:46 +0100

    Fix markdown

--- Defaults/Markdown.tsx
+++ Defaults/Markdown.tsx
@@ -1,15 +1,16 @@
 import { template } from "uix/html/template.ts";
 import { Component } from "uix/components/Component.ts";
 import 'https://cdn.jsdelivr.net/npm/marked@9.1.2/+esm';
+import { Datex } from "unyt_core/datex.ts";
 declare const marked: any;
 
-@template(function(this: Markdown) {
+@blankTemplate<{}, Datex.RefOrValue<string>>(({children}) => {
 	return <>
-		{this.options.value?.replaceAll("\n", "\\n") || ''}
+		{always(()=>{
+			marked(val(children)?.replaceAll("\n", "\\n") || '')
+		})}
 	</>
 })
-export default class Markdown extends Component<{value?: string}> {
-	protected override onCreate(): void|Promise<void> {
-		this.innerHTML = marked(this.innerText.replaceAll("\\n", "\n"));
-	}
+export default class Markdown extends Component<{value?: Datex.Ref<string>}> {
+	
 }
